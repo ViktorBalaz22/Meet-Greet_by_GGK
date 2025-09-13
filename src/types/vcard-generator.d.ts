33838be07a3e7017593a2a4ef62c00dfd78e4932
ViktorBalaz22 <viktor.balaz22@gmail.com>
2025-09-13 12:26:41 +0200

    Add TypeScript types for qrcode and vcard-generator libraries

---
+++ src/types/vcard-generator.d.ts
@@ -0,0 +1,16 @@
+declare module 'vcard-generator' {
+  interface VCardOptions {
+    name?: string
+    firstName?: string
+    lastName?: string
+    organization?: string
+    title?: string
+    phone?: string
+    email?: string
+    url?: string
+    note?: string
+  }
+
+  function vCardGenerator(options: VCardOptions): string
+  export = vCardGenerator
+}
